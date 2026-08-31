7ebe987c71af0b4772d29a7c786617519cb18fcb
breichen <benjamin24@gmx.at>
2026-08-31 12:25:20 +0200

    Update allegrid.js

--- js/allegrid.js
+++ js/allegrid.js
@@ -3,5 +3,14 @@
   Nutzt baueBuchKachel() aus js/buchgrid.js, damit die Kacheln
   überall exakt gleich aussehen.
 */
+
+// Nur Ratgeber anzeigen, die ein "erstellt"-Datum haben UND dessen
+// Datum bereits erreicht ist (heute oder in der Vergangenheit) -
+// siehe istDatumErreicht() in js/datumsformat.js. Ratgeber ohne
+// Datum oder mit einem Datum in der Zukunft werden ausgeblendet.
+const sichtbareRatgeber = ratgeberListe.filter(
+  buch => buch.erstellt && istDatumErreicht(buch.erstellt)
+);
+
 document.getElementById("alle-ratgeber").innerHTML =
-  ratgeberListe.map(baueBuchKachel).join("");
+  sichtbareRatgeber.map(baueBuchKachel).join("");
