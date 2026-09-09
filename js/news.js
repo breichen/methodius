17babf90247e0e0f2760fa8de92dee1519ee35c0
breichen <benjamin24@gmx.at>
2026-09-09 21:22:54 +0200

    visually separate news

--- js/news.js
+++ js/news.js
@@ -20,6 +20,9 @@ const NewsKategorie = Object.freeze({
 
 const newsListe = [
 
+
+  // PAPERS
+
   {
     datei: "neues-paper-problem-tatsaechlich-geloest.md",
     titel: "Neues Paper veröffentlicht",
@@ -75,16 +78,6 @@ const newsListe = [
   },
 
   {
-    datei: "teamevent-oper.md",
-    titel: "Teamevent im Nationaltheater",
-    datum: "2027-10-28",
-    bild: "pics/news/oper.png",
-    link: "institutsleben.html",
-    linkText: "Zum Institutsleben",
-    kategorie: NewsKategorie.INSTITUTSLEBEN,
-  },
-
-  {
     datei: "neues-paper-gute-vorsaetze-bevorzugt.md",
     titel: "Neues Paper veröffentlicht",
     datum: "2027-09-09",
@@ -102,31 +95,6 @@ const newsListe = [
     kategorie: NewsKategorie.VEROEFFENTLICHUNGEN,
   },
 
-  {
-    datei: "methodius-orden.md",
-    titel: "Hohe Auszeichnung für Dr. Maximilian Methodius",
-    datum: "2027-06-12",
-    bild: "pics/news/methodius-orden.png",
-    kategorie: NewsKategorie.INSTITUTSLEBEN,
-  },
-
-  {
-    datei: "teamevent-hiking.md",
-    titel: "Teamevent in den Bergen",
-    datum: "2027-06-05",
-    bild: "pics/news/hiking.png",
-    link: "institutsleben.html",
-    linkText: "Zum Institutsleben",
-    kategorie: NewsKategorie.INSTITUTSLEBEN,
-  },
-
-  {
-    datei: "huber-carl-von-frisch-medaille.md",
-    titel: "Verleihung der Carl von Frisch-Medaille an Dr. Konrad P. Huber",
-    datum: "2027-05-24",
-    bild: "pics/news/huber-carl-von-frisch-medaille.png",
-    kategorie: NewsKategorie.INSTITUTSLEBEN,
-  },
 
   {
     datei: "neues-paper-die-rolle-unbequemer-einwaende.md",
@@ -136,15 +104,6 @@ const newsListe = [
     linkText: "Zu den Veröffentlichungen",
     kategorie: NewsKategorie.VEROEFFENTLICHUNGEN,
   },
-
-  {
-    datei: "wankelmuth-sigismund-freud-medaille.md",
-    titel: "Verleihung der Sigismund-Freud-Medaille an Prof. Dr. Hildegard Wankelmuth",
-    datum: "2027-04-26",
-    bild: "pics/news/wankelmuth-sigismund-freud-medaille.png",
-    kategorie: NewsKategorie.INSTITUTSLEBEN,
-  },
-
   {
     datei: "neues-paper-warum-menschen-ratschlaege-suchen.md",
     titel: "Neues Paper veröffentlicht",
@@ -152,16 +111,6 @@ const newsListe = [
     link: "veroeffentlichungen.html",
     linkText: "Zu den Veröffentlichungen",
     kategorie: NewsKategorie.VEROEFFENTLICHUNGEN,
-  },
-
-  {
-    datei: "teamevent-billard.md",
-    titel: "Teamevent am Billardtisch",
-    datum: "2027-01-21",
-    bild: "pics/news/billard.png",
-    link: "institutsleben.html",
-    linkText: "Zum Institutsleben",
-    kategorie: NewsKategorie.INSTITUTSLEBEN,
   },
 
   {
@@ -174,79 +123,11 @@ const newsListe = [
   },
 
   {
-    datei: "jahrestagung-2026.md",
-    titel: "Methodius-Institut richtet erfolgreiche Fachtagung aus",
-    datum: "2026-11-15",
-    bild: "pics/news/jahrestagung-2026.png",
-    link: "institutsleben.html",
-    linkText: "Zum Institutsleben",
-    kategorie: NewsKategorie.INSTITUTSLEBEN,
-  },
-
-  {
     datei: "neues-paper-vorlaeufige-erkenntnisse-kontr-ueberinterpretation.md",
     titel: "Neues Paper veröffentlicht",
     datum: "2026-10-17",
     link: "veroeffentlichungen.html",
     linkText: "Zu den Veröffentlichungen",
-    kategorie: NewsKategorie.VEROEFFENTLICHUNGEN,
-  },
-
-  {
-    datei: "erkenntnis-windows-suche.md",
-    titel: "Auffälligkeiten bei der lokalen Programmsuche",
-    datum: "2026-10-12",
-    bild: "pics/news/erkenntnis-windows-suche.png",
-    kategorie: NewsKategorie.KURIOSITAETEN,
-  },
-
-  {
-    datei: "jahrestagung-2026-poster.md",
-    titel: "Ankündigung der Jahrestagung 2026",
-    datum: "2026-09-29",
-    bild: "pics/news/jahrestagung-2026-poster.png",
-    link: "institutsleben.html",
-    linkText: "Zum Institutsleben",
-    kategorie: NewsKategorie.INSTITUTSLEBEN,
-  },
-
-  {
-    datei: "teamevent-rafting.md",
-    titel: "Teamevent auf der Moldau",
-    datum: "2026-09-15",
-    bild: "pics/news/rafting.png",
-    link: "institutsleben.html",
-    linkText: "Zum Institutsleben",
-    kategorie: NewsKategorie.INSTITUTSLEBEN,
-  },
-
-  {
-    datei: "erkenntnis-amazon-cheap-cds.md",
-    titel: "Unstimmigkeit in der Produktsuche",
-    datum: "2026-09-05",
-    bild: "pics/news/erkenntnis-amazon-cheap-cds.png",
-    link: "https://www.amazon.de/s?i=popular&rh=n%3A255882%2Cp_36%3A-500&s=featured-rank&content-id=amzn1.sym.fc4f3753-af8e-4288-9354-f00cdcb2b673&pd_rd_r=90bcb95b-5d01-406b-a1b8-edb1ee93c9f9&pd_rd_w=M0pmc&pd_rd_wg=jTSWs&pf_rd_p=fc4f3753-af8e-4288-9354-f00cdcb2b673&pf_rd_r=NERF60648PVA1X44KV0X&ref=Oct_d_oup_S",
-    linkText: "Überprüfe die aktuellen Treffer",
-    kategorie: NewsKategorie.KURIOSItAETEN,
-  },
-
-  {
-    datei: "erkenntnis-amazon-cd-preise.md",
-    titel: "Preisabweichungen im Versandhandel",
-    datum: "2026-08-31",
-    bild: "pics/news/erkenntnis-amazon-cd-preise.png",
-    link: "https://www.amazon.de/Shape-Fluidity-Digipak-Dool/dp/B08LS9VYGF/ref=sr_1_3?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=V5IBWTPQ120Z&dib=eyJ2IjoiMSJ9.5w82_zdt_kO8RzGRNLIo6xotFtzsR9VLpULHdVVCPqcznzdWnU5XyYWYllIl368Atr9pWDmOnraBxUeoZop72-LWCsdt8RaBR0akJpXEpN-tP21LcNJZLglB4-WhM1B9hvB_DIyRXoWtkwYmdNatGYcTnvGEZqxTwGB4wrN6YiIX_hg47-4ezksixawYy3yvw4xyIvFpVfBPoe5J393igUhPFM9s3C2sbqeesIvYwlU.MpOwAxiTsuE27XJJHgDw8_csAPylUfKBEugTRve5CYI&dib_tag=se&keywords=dool+cd&qid=1788173090&sprefix=dool+cd%2Caps%2C109&sr=8-3",
-    linkText: "Überprüfe einen Preis",
-    kategorie: NewsKategorie.KURIOSItAETEN,
-  },
-
-  {
-    datei: "neuer-ratgeber-muskelabbau.md",
-    titel: "Neuer Ratgeber erschienen",
-    datum: "2026-08-30",
-    bild: "pics/ratgeber/Abnehmen dank Muskelabbau.png",
-    link: "buch.html?titel=Abnehmen dank Muskelabbau",
-    linkText: "Zum Ratgeber",
     kategorie: NewsKategorie.VEROEFFENTLICHUNGEN,
   },
 
@@ -260,15 +141,6 @@ const newsListe = [
   },
 
   {
-    datei: "neuer-fall-ans-bett-gebunden.md",
-    titel: "Neue Fallakte angelegt",
-    datum: "2026-08-27",
-    link: "problem.html?datei=ans-bett-gebunden.md",
-    linkText: "Zur Fallakte",
-    kategorie: NewsKategorie.VEROEFFENTLICHUNGEN,
-  },
-
-  {
     datei: "neues-paper-grenzen-der-wiss-ueberinterpretation.md",
     titel: "Neues Paper veröffentlicht",
     datum: "2026-08-17",
@@ -276,15 +148,6 @@ const newsListe = [
     linkText: "Zu den Veröffentlichungen",
     kategorie: NewsKategorie.VEROEFFENTLICHUNGEN,
   },
-
-  {
-    datei: "erkenntnis-hofer-limonade-preise.md",
-    titel: "Bemerkenswerte Preisgestaltung im Discounter",
-    datum: "2026-08-06",
-    bild: "pics/news/erkenntnis-hofer-limonade-preise.png",
-    kategorie: NewsKategorie.KURIOSItAETEN,
-  },
-
 
   {
     datei: "neues-paper-korrelation-kausalitaet.md",
@@ -340,6 +203,118 @@ const newsListe = [
     kategorie: NewsKategorie.VEROEFFENTLICHUNGEN,
   },
 
+
+  // RATGEBER
+
+  {
+    datei: "neuer-ratgeber-muskelabbau.md",
+    titel: "Neuer Ratgeber erschienen",
+    datum: "2026-08-30",
+    bild: "pics/ratgeber/Abnehmen dank Muskelabbau.png",
+    link: "buch.html?titel=Abnehmen dank Muskelabbau",
+    linkText: "Zum Ratgeber",
+    kategorie: NewsKategorie.VEROEFFENTLICHUNGEN,
+  },
+
+
+  // FALLAKTEN
+
+  {
+    datei: "neuer-fall-ans-bett-gebunden.md",
+    titel: "Neue Fallakte angelegt",
+    datum: "2026-08-27",
+    link: "problem.html?datei=ans-bett-gebunden.md",
+    linkText: "Zur Fallakte",
+    kategorie: NewsKategorie.VEROEFFENTLICHUNGEN,
+  },
+
+
+  // INSTITUTSLEBEN
+
+  {
+    datei: "teamevent-oper.md",
+    titel: "Teamevent im Nationaltheater",
+    datum: "2027-10-28",
+    bild: "pics/news/oper.png",
+    link: "institutsleben.html",
+    linkText: "Zum Institutsleben",
+    kategorie: NewsKategorie.INSTITUTSLEBEN,
+  },
+
+  {
+    datei: "methodius-orden.md",
+    titel: "Hohe Auszeichnung für Dr. Maximilian Methodius",
+    datum: "2027-06-12",
+    bild: "pics/news/methodius-orden.png",
+    kategorie: NewsKategorie.INSTITUTSLEBEN,
+  },
+
+  {
+    datei: "teamevent-hiking.md",
+    titel: "Teamevent in den Bergen",
+    datum: "2027-06-05",
+    bild: "pics/news/hiking.png",
+    link: "institutsleben.html",
+    linkText: "Zum Institutsleben",
+    kategorie: NewsKategorie.INSTITUTSLEBEN,
+  },
+
+  {
+    datei: "huber-carl-von-frisch-medaille.md",
+    titel: "Verleihung der Carl von Frisch-Medaille an Dr. Konrad P. Huber",
+    datum: "2027-05-24",
+    bild: "pics/news/huber-carl-von-frisch-medaille.png",
+    kategorie: NewsKategorie.INSTITUTSLEBEN,
+  },
+
+  {
+    datei: "wankelmuth-sigismund-freud-medaille.md",
+    titel: "Verleihung der Sigismund-Freud-Medaille an Prof. Dr. Hildegard Wankelmuth",
+    datum: "2027-04-26",
+    bild: "pics/news/wankelmuth-sigismund-freud-medaille.png",
+    kategorie: NewsKategorie.INSTITUTSLEBEN,
+  },
+
+  {
+    datei: "teamevent-billard.md",
+    titel: "Teamevent am Billardtisch",
+    datum: "2027-01-21",
+    bild: "pics/news/billard.png",
+    link: "institutsleben.html",
+    linkText: "Zum Institutsleben",
+    kategorie: NewsKategorie.INSTITUTSLEBEN,
+  },
+
+  {
+    datei: "jahrestagung-2026.md",
+    titel: "Methodius-Institut richtet erfolgreiche Fachtagung aus",
+    datum: "2026-11-15",
+    bild: "pics/news/jahrestagung-2026.png",
+    link: "institutsleben.html",
+    linkText: "Zum Institutsleben",
+    kategorie: NewsKategorie.INSTITUTSLEBEN,
+  },
+
+  {
+    datei: "jahrestagung-2026-poster.md",
+    titel: "Ankündigung der Jahrestagung 2026",
+    datum: "2026-09-29",
+    bild: "pics/news/jahrestagung-2026-poster.png",
+    link: "institutsleben.html",
+    linkText: "Zum Institutsleben",
+    kategorie: NewsKategorie.INSTITUTSLEBEN,
+  },
+
+  {
+    datei: "teamevent-rafting.md",
+    titel: "Teamevent auf der Moldau",
+    datum: "2026-09-15",
+    bild: "pics/news/rafting.png",
+    link: "institutsleben.html",
+    linkText: "Zum Institutsleben",
+    kategorie: NewsKategorie.INSTITUTSLEBEN,
+  },
+
   {
     datei: "gruendung-methodius-institut.md",
     titel: "Methodius-Institut für Lebenswissenschaften gegründet",
@@ -348,6 +323,45 @@ const newsListe = [
     link: "institutsleben.html",
     linkText: "Zum Institutsleben",
     kategorie: NewsKategorie.INSTITUTSLEBEN,
+  },
+
+
+  // KURIOSITAETEN
+
+  {
+    datei: "erkenntnis-windows-suche.md",
+    titel: "Auffälligkeiten bei der lokalen Programmsuche",
+    datum: "2026-10-12",
+    bild: "pics/news/erkenntnis-windows-suche.png",
+    kategorie: NewsKategorie.KURIOSITAETEN,
+  },
+
+  {
+    datei: "erkenntnis-amazon-cheap-cds.md",
+    titel: "Unstimmigkeit in der Produktsuche",
+    datum: "2026-09-05",
+    bild: "pics/news/erkenntnis-amazon-cheap-cds.png",
+    link: "https://www.amazon.de/s?i=popular&rh=n%3A255882%2Cp_36%3A-500&s=featured-rank&content-id=amzn1.sym.fc4f3753-af8e-4288-9354-f00cdcb2b673&pd_rd_r=90bcb95b-5d01-406b-a1b8-edb1ee93c9f9&pd_rd_w=M0pmc&pd_rd_wg=jTSWs&pf_rd_p=fc4f3753-af8e-4288-9354-f00cdcb2b673&pf_rd_r=NERF60648PVA1X44KV0X&ref=Oct_d_oup_S",
+    linkText: "Überprüfe die aktuellen Treffer",
+    kategorie: NewsKategorie.KURIOSITAETEN,
+  },
+
+  {
+    datei: "erkenntnis-amazon-cd-preise.md",
+    titel: "Preisabweichungen im Versandhandel",
+    datum: "2026-08-31",
+    bild: "pics/news/erkenntnis-amazon-cd-preise.png",
+    link: "https://www.amazon.de/Shape-Fluidity-Digipak-Dool/dp/B08LS9VYGF/ref=sr_1_3?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=V5IBWTPQ120Z&dib=eyJ2IjoiMSJ9.5w82_zdt_kO8RzGRNLIo6xotFtzsR9VLpULHdVVCPqcznzdWnU5XyYWYllIl368Atr9pWDmOnraBxUeoZop72-LWCsdt8RaBR0akJpXEpN-tP21LcNJZLglB4-WhM1B9hvB_DIyRXoWtkwYmdNatGYcTnvGEZqxTwGB4wrN6YiIX_hg47-4ezksixawYy3yvw4xyIvFpVfBPoe5J393igUhPFM9s3C2sbqeesIvYwlU.MpOwAxiTsuE27XJJHgDw8_csAPylUfKBEugTRve5CYI&dib_tag=se&keywords=dool+cd&qid=1788173090&sprefix=dool+cd%2Caps%2C109&sr=8-3",
+    linkText: "Überprüfe einen Preis",
+    kategorie: NewsKategorie.KURIOSITAETEN,
+  },
+
+  {
+    datei: "erkenntnis-hofer-limonade-preise.md",
+    titel: "Bemerkenswerte Preisgestaltung im Discounter",
+    datum: "2026-08-06",
+    bild: "pics/news/erkenntnis-hofer-limonade-preise.png",
+    kategorie: NewsKategorie.KURIOSITAETEN,
   },
 
 ];
