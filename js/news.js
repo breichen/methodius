a3d75a918c411501833b099ec7b9c0b6bb7755ce
breichen <benjamin24@gmx.at>
2026-09-18 10:26:26 +0200

    move news for institute and curiosities to files

--- js/news.js
+++ js/news.js
@@ -46,142 +46,11 @@ const newsListe = [
   },
 
 
-  // INSTITUTSLEBEN
-
-  {
-    datei: "teamevent-oper.md",
-    titel: "Teamevent im Nationaltheater",
-    datum: "2027-10-28",
-    bild: "pics/news/oper.png",
-    link: "institutsleben.html",
-    linkText: "Zum Institutsleben",
-    kategorie: NewsKategorie.INSTITUTSLEBEN,
-  },
-
-  {
-    datei: "methodius-orden.md",
-    titel: "Hohe Auszeichnung für Dr. Maximilian Methodius",
-    datum: "2027-06-12",
-    bild: "pics/news/methodius-orden.png",
-    kategorie: NewsKategorie.INSTITUTSLEBEN,
-  },
-
-  {
-    datei: "teamevent-hiking.md",
-    titel: "Teamevent in den Bergen",
-    datum: "2027-06-05",
-    bild: "pics/news/hiking.png",
-    link: "institutsleben.html",
-    linkText: "Zum Institutsleben",
-    kategorie: NewsKategorie.INSTITUTSLEBEN,
-  },
-
-  {
-    datei: "huber-carl-von-frisch-medaille.md",
-    titel: "Verleihung der Carl von Frisch-Medaille an Dr. Konrad P. Huber",
-    datum: "2027-05-24",
-    bild: "pics/news/huber-carl-von-frisch-medaille.png",
-    kategorie: NewsKategorie.INSTITUTSLEBEN,
-  },
-
-  {
-    datei: "wankelmuth-sigismund-freud-medaille.md",
-    titel: "Verleihung der Sigismund-Freud-Medaille an Prof. Dr. Hildegard Wankelmuth",
-    datum: "2027-04-26",
-    bild: "pics/news/wankelmuth-sigismund-freud-medaille.png",
-    kategorie: NewsKategorie.INSTITUTSLEBEN,
-  },
-
-  {
-    datei: "teamevent-billard.md",
-    titel: "Teamevent am Billardtisch",
-    datum: "2027-01-21",
-    bild: "pics/news/billard.png",
-    link: "institutsleben.html",
-    linkText: "Zum Institutsleben",
-    kategorie: NewsKategorie.INSTITUTSLEBEN,
-  },
-
-  {
-    datei: "jahrestagung-2026.md",
-    titel: "Methodius-Institut richtet erfolgreiche Fachtagung aus",
-    datum: "2026-11-15",
-    bild: "pics/news/jahrestagung-2026.png",
-    link: "institutsleben.html",
-    linkText: "Zum Institutsleben",
-    kategorie: NewsKategorie.INSTITUTSLEBEN,
-  },
-
-  {
-    datei: "jahrestagung-2026-poster.md",
-    titel: "Ankündigung der Jahrestagung 2026",
-    datum: "2026-09-29",
-    bild: "pics/news/jahrestagung-2026-poster.png",
-    link: "institutsleben.html",
-    linkText: "Zum Institutsleben",
-    kategorie: NewsKategorie.INSTITUTSLEBEN,
-  },
-
-  {
-    datei: "teamevent-rafting.md",
-    titel: "Teamevent auf der Moldau",
-    datum: "2026-09-15",
-    bild: "pics/news/rafting.png",
-    link: "institutsleben.html",
-    linkText: "Zum Institutsleben",
-    kategorie: NewsKategorie.INSTITUTSLEBEN,
-  },
-
-  {
-    datei: "gruendung-methodius-institut.md",
-    titel: "Methodius-Institut für Lebenswissenschaften gegründet",
-    datum: "2026-09-04",
-    bild: "pics/news/gruendung-methodius-institut.png",
-    link: "institutsleben.html",
-    linkText: "Zum Institutsleben",
-    kategorie: NewsKategorie.INSTITUTSLEBEN,
-  },
-
-
-  // KURIOSITAETEN
-
-  {
-    datei: "erkenntnis-windows-suche.md",
-    titel: "Auffälligkeiten bei der lokalen Programmsuche",
-    datum: "2026-10-12",
-    bild: "pics/news/erkenntnis-windows-suche.png",
-    kategorie: NewsKategorie.KURIOSITAETEN,
-  },
-
-  {
-    datei: "erkenntnis-amazon-cheap-cds.md",
-    titel: "Unstimmigkeit in der Produktsuche",
-    datum: "2026-09-05",
-    bild: "pics/news/erkenntnis-amazon-cheap-cds.png",
-    link: "https://www.amazon.de/s?i=popular&rh=n%3A255882%2Cp_36%3A-500&s=featured-rank&content-id=amzn1.sym.fc4f3753-af8e-4288-9354-f00cdcb2b673&pd_rd_r=90bcb95b-5d01-406b-a1b8-edb1ee93c9f9&pd_rd_w=M0pmc&pd_rd_wg=jTSWs&pf_rd_p=fc4f3753-af8e-4288-9354-f00cdcb2b673&pf_rd_r=NERF60648PVA1X44KV0X&ref=Oct_d_oup_S",
-    linkText: "Überprüfe die aktuellen Treffer",
-    kategorie: NewsKategorie.KURIOSITAETEN,
-  },
-
-  {
-    datei: "erkenntnis-amazon-cd-preise.md",
-    titel: "Preisabweichungen im Versandhandel",
-    datum: "2026-08-31",
-    bild: "pics/news/erkenntnis-amazon-cd-preise.png",
-    link: "https://www.amazon.de/Shape-Fluidity-Digipak-Dool/dp/B08LS9VYGF/ref=sr_1_3?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=V5IBWTPQ120Z&dib=eyJ2IjoiMSJ9.5w82_zdt_kO8RzGRNLIo6xotFtzsR9VLpULHdVVCPqcznzdWnU5XyYWYllIl368Atr9pWDmOnraBxUeoZop72-LWCsdt8RaBR0akJpXEpN-tP21LcNJZLglB4-WhM1B9hvB_DIyRXoWtkwYmdNatGYcTnvGEZqxTwGB4wrN6YiIX_hg47-4ezksixawYy3yvw4xyIvFpVfBPoe5J393igUhPFM9s3C2sbqeesIvYwlU.MpOwAxiTsuE27XJJHgDw8_csAPylUfKBEugTRve5CYI&dib_tag=se&keywords=dool+cd&qid=1788173090&sprefix=dool+cd%2Caps%2C109&sr=8-3",
-    linkText: "Überprüfe einen Preis",
-    kategorie: NewsKategorie.KURIOSITAETEN,
-  },
-
-  {
-    datei: "erkenntnis-hofer-limonade-preise.md",
-    titel: "Bemerkenswerte Preisgestaltung im Discounter",
-    datum: "2026-08-06",
-    bild: "pics/news/erkenntnis-hofer-limonade-preise.png",
-    kategorie: NewsKategorie.KURIOSITAETEN,
-  },
-
 ];
+
+// Institutsleben- und Kuriositäten-Einträge liegen jeweils in einer
+// eigenen JSON-Datei (siehe data/institutsleben.json und
+// data/kuriositaeten.json) und werden in ladeAlleNews() dazugeladen.
 
 
 
@@ -195,6 +64,30 @@ function erzeugePaperNews(veroeffentlichungen) {
     linkText: "Zur Veröffentlichung",
     kategorie: NewsKategorie.VEROEFFENTLICHUNGEN,
   }));
+}
+
+// Versieht eine Liste roher News-Einträge (ohne kategorie-Feld) mit
+// der übergebenen Kategorie. Wird für die ausgelagerten JSON-Dateien
+// (Institutsleben, Kuriositäten) verwendet.
+function versieheMitKategorie(eintraege, kategorie) {
+  return eintraege.map(eintrag => ({
+    ...eintrag,
+    kategorie,
+  }));
+}
+
+async function ladeJsonNews(pfad, kategorie) {
+
+  const response = await fetch(pfad);
+
+  if (!response.ok) {
+    throw new Error(`News-Datei nicht gefunden: ${pfad}`);
+  }
+
+  const eintraege = await response.json();
+
+  return versieheMitKategorie(eintraege, kategorie);
+
 }
 
 async function ladeAlleNews() {
@@ -214,9 +107,16 @@ async function ladeAlleNews() {
   const paperNews =
     erzeugePaperNews(veroeffentlichungen);
 
+  const [institutslebenNews, kuriositaetenNews] = await Promise.all([
+    ladeJsonNews("data/institutsleben.json", NewsKategorie.INSTITUTSLEBEN),
+    ladeJsonNews("data/kuriositaeten.json", NewsKategorie.KURIOSITAETEN),
+  ]);
+
   return [
     ...newsListe,
-    ...paperNews
+    ...paperNews,
+    ...institutslebenNews,
+    ...kuriositaetenNews,
   ];
 
 }
